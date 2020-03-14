2a1d94b3326ad50fa4f1eb51fc2eec1f58cc680f
Pedro <pedrohba18@gmail.com>
2020-03-13 21:46:16 -0300

    global Style

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import history from './services/history';
 import Routes from './routes';
-
 import './config/ReactotronConfig';
+import GlobalStyle from './styles/global';
 
 function App() {
     return (
         <Router history={history}>
             <Routes />
+            <GlobalStyle />
         </Router>
     );
 }
