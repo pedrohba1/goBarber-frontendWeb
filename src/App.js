335283ef7623656220a72bc7b8f5378343dcaf39
pedro Bufulin <pedro.almeida@ufu.br>
2020-03-16 22:22:51 -0300

    configurando o redux, redux-saga,  e o store

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import history from './services/history';
 import Routes from './routes';
 import './config/ReactotronConfig';
 import GlobalStyle from './styles/global';
 
+// esse import store tem que vir depois da importação do reactotorn
+import store from './store';
+
 function App() {
     return (
-        <Router history={history}>
-            <Routes />
-            <GlobalStyle />
-        </Router>
+        <Provider store={store}>
+            <Router history={history}>
+                <Routes />
+                <GlobalStyle />
+            </Router>
+        </Provider>
     );
 }
 
