accd1fcd263f139f0f30ca102d5649a11ca5f030
pedro Bufulin <pedro.almeida@ufu.br>
2020-03-18 12:50:41 -0300

    header

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Content, Profile } from './styles';
+import logo from '~/assets/logo_nav.svg';
+
+export default function Header() {
+    return (
+        <Container>
+            <Content>
+                <nav>
+                    <img src={logo} alt="goBarber" />
+                    <Link to="/dashboard">DASHBOARD</Link>
+                </nav>
+
+                <aside>
+                    <Profile>
+                        <div>
+                            <strong>Pedro Bufulin</strong>
+                            <Link to="/profile">Meu perfil</Link>
+                        </div>
+                        <img
+                            src="https://api.adorable.io/avatars/50/abott@adorable.pngCopy to Clipboard"
+                            alt="perfil"
+                        />
+                    </Profile>
+                </aside>
+            </Content>
+        </Container>
+    );
+}
