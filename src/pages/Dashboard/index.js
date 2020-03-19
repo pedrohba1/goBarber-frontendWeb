35786c0411f20f3d98b3647dbba1edf7a5de4f41
pedro Bufulin <pedro.almeida@ufu.br>
2020-03-19 14:31:36 -0300

    estilização do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
-// import { Container } from './styles';
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-    api.get('appointments');
+    return (
+        <Container>
+            <header>
+                <button type="button">
+                    <MdChevronLeft size={36} color="#fff " />
+                </button>
+                <strong>31 de maio</strong>
+                <button type="button">
+                    <MdChevronRight size={36} color="#fff " />
+                </button>
+            </header>
 
-    return <h1>dashboard</h1>;
+            <ul>
+                <Time past>
+                    <strong> 08:00</strong>
+                    <span>Pedro Bufulin</span>
+                </Time>
+                <Time available>
+                    <strong>09:00</strong>
+                    <span>Em aberto</span>
+                </Time>
+                <Time>
+                    <strong> 10:00</strong>
+                    <span>Pedro Bufulin</span>
+                </Time>
+                <Time>
+                    <strong> 11:00</strong>
+                    <span>Pedro Bufulin</span>
+                </Time>
+            </ul>
+        </Container>
+    );
 }
