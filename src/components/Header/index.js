5db84e9182d241d838c7f974357d372ea31df552
pedro Bufulin <pedro.almeida@ufu.br>
2020-03-18 21:40:31 -0300

    display de informações no header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Container, Content, Profile } from './styles';
 import logo from '~/assets/logo_nav.svg';
@@ -6,6 +7,8 @@ import logo from '~/assets/logo_nav.svg';
 import Notifications from '~/components/Notifications';
 
 export default function Header() {
+    const profile = useSelector(state => state.user.profile);
+
     return (
         <Container>
             <Content>
@@ -19,11 +22,14 @@ export default function Header() {
 
                     <Profile>
                         <div>
-                            <strong>Pedro Bufulin</strong>
+                            <strong>{profile.name}</strong>
                             <Link to="/profile">Meu perfil</Link>
                         </div>
                         <img
-                            src="https://api.adorable.io/avatars/50/abott@adorable.pngCopy to Clipboard"
+                            src={
+                                profile.avatar.url ||
+                                'https://api.adorable.io/avatars/50/abott@adorable.pngCopy to Clipboard'
+                            }
                             alt="perfil"
                         />
                     </Profile>
